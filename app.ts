ef4167ef75b59370a9998a581be6c6c1685f1a6b
gwonyejun <kyjuni1059@gmail.com>
2023-06-09 09:49:20 +0900

    express를 ts 방식으로 서버를 열어 보았다.

---
+++ app.ts
@@ -0,0 +1,25 @@
+import express ,{Request , Response}from "express";
+import path from "path";
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("*", (req : Request, res : Response) => {
+  res.type("text/html");
+  res.sendFile(__dirname + "/index.html");
+});
+
+// 404 페이지 작성
+app.use((req : Request, res : Response) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not Found");
+});
+
+
+// 서버 포트 번호 이다.
+app.listen(7777, () => {
+  console.log(`Listening on port 열결 성공`);
+});
