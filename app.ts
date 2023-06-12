e405a4f290743cc432abece8340e4a2cce06931e
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-12 10:16:27 +0900

    예준님 브랜치와 병합

--- app.ts
+++ app.ts
@@ -1,25 +1,23 @@
-import express ,{Request , Response}from "express";
+import express, { Request, Response } from "express";
 import path from "path";
 
 const app = express();
 
-
 app.use(express.static(path.join(__dirname)));
 
-app.get("*", (req : Request, res : Response) => {
+app.get("*", (req: Request, res: Response) => {
   res.type("text/html");
   res.sendFile(__dirname + "/index.html");
 });
 
 // 404 페이지 작성
-app.use((req : Request, res : Response) => {
+app.use((req: Request, res: Response) => {
   res.type("text/plain");
   res.status(404);
   res.send("404 - Not Found");
 });
 
-
 // 서버 포트 번호 이다.
 app.listen(7777, () => {
-  console.log(`Listening on port 열결 성공`);
+  console.log(`연결 성공`);
 });
