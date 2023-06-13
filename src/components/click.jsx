c476d5efc212cf145e838fc84138cbf6009caea0
gwonyejun <kyjuni1059@gmail.com>
2023-06-13 19:31:46 +0900

    paper 예제 첨부

--- src/components/click.jsx
+++ src/components/click.jsx
@@ -1,6 +1,30 @@
-// 리액트 라이브러리
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from 'react';
+import { PaperScope, Path } from 'paper';
 
-// 리액트 컴포넌트
+function PaperCanvas() {
+  const canvasRef = useRef(null);
 
-export default function Click() {}
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const paperScope = new PaperScope(canvas);
+
+    // 캔버스 크기 설정
+    paperScope.setup(canvas);
+
+    // 경로 생성
+    const path = new Path.Rectangle({
+      point: [50, 50],
+      size: [200, 100],
+      fillColor: 'red',
+    });
+
+    // 경로 그리기
+    paperScope.view.onFrame = () => {
+      path.rotate(1);
+    };
+  }, []);
+
+  return <canvas ref={canvasRef} />;
+}
+
+export default PaperCanvas;
