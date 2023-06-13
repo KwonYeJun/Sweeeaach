d1d843ab4dfd7ed419ccf71e0f4b31812db0a710
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-13 19:26:49 +0900

    paper.js를 우선 제거, react-svg를 사용해보기로 함

---
+++ src/components/click.jsx
@@ -0,0 +1,6 @@
+// 리액트 라이브러리
+import React, { useEffect } from "react";
+
+// 리액트 컴포넌트
+
+export default function Click() {}
--- src/components/selectSwitch.jsx
+++ src/components/selectSwitch.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Click from "./click"
 
 export default function () {
   return (
     <div className="switches">
+      <Click/>
       <div className="click">클릭</div>
       <div className="tactile">택타일</div>
       <div className="linear">리니어</div>
