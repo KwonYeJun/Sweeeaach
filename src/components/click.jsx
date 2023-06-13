8ff8a8508b82ae6a90fecdf614343770066c2e45
krapli441 <tlfqj1208@gmail.com>
2023-06-13 21:10:08 +0900

    작업 전환 전 커밋

--- src/components/click.jsx
+++ src/components/click.jsx
@@ -1,28 +1,29 @@
-import React, { useEffect, useRef } from 'react';
-import paper from 'paper';
+import React, { useEffect, useRef } from "react";
+import paper from "paper";
+import { PointText } from "paper/dist/paper-core";
 
 function PaperCanvas() {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      console.log('키 입력 감지됨');
+      console.log("키 입력 감지됨");
       playSound();
     };
 
     const handleKeyUp = (event) => {
-      console.log('키 입력 감지됨');
+      console.log("키 입력 감지됨");
       playSoundTwo();
     };
 
     const playSound = () => {
-      const clickOnPush = new Audio('/audio/keysoundTest/clickonPush.m4a');
+      const clickOnPush = new Audio("/audio/keysoundTest/clickonPush.m4a");
       clickOnPush.play();
       clickOnPush.volume = Math.random();
     };
 
     const playSoundTwo = () => {
-      const clickOnLeave = new Audio('/audio/keysoundTest/clickleavePush.m4a');
+      const clickOnLeave = new Audio("/audio/keysoundTest/clickleavePush.m4a");
       clickOnLeave.play();
       clickOnLeave.volume = Math.random();
     };
@@ -36,25 +37,28 @@ function PaperCanvas() {
     let tool;
     let backSpace;
 
-    paperScope.project.importSVG('/SVG/Keyboard.svg', (item) => {
-      console.log(item);
-      const centerX = paperScope.view.center.x;
-      const centerY = paperScope.view.center.y;
-      item.position = new paperScope.Point(centerX, centerY);
-      const switchHouse = Object.values(item.children[2]._children);
-      backSpace = item.children[2]._children.BackSpace;
-      tool = new paperScope.Tool();
+    const keyboard = paperScope.project.importSVG(
+      "/SVG/Keyboard.svg",
+      (item) => {
+        console.log(item);
+        const centerX = paperScope.view.center.x;
+        const centerY = paperScope.view.center.y;
+        item.position = new paperScope.Point(centerX, centerY);
+        const switchHouse = Object.values(item.children[2]._children);
+        backSpace = item.children[2]._children.BackSpace;
+        tool = new paperScope.Tool();
 
-      backSpace.onKeyDown = tool.onKeyDown;
-      tool.onKeyDown = handleKeyDown;
-    });
+        backSpace.onKeyDown = tool.onKeyDown;
+        tool.onKeyDown = handleKeyDown;
+      }
+    );
 
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('keyup', handleKeyUp);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
 
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.removeEventListener('keyup', handleKeyUp);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
     };
   }, []);
 
