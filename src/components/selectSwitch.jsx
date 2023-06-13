8a9870f21b01ce1956b6a9d64353a8f81e7a87a1
krapli441 <tlfqj1208@gmail.com>
2023-06-13 21:18:13 +0900

    react-svg를 사용하여 요소를 불러왔다.

--- src/components/selectSwitch.jsx
+++ src/components/selectSwitch.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Click from "./click"
+import SVGclick from "./svgClick"
 
 export default function () {
   return (
     <div className="switches">
-      <Click/>
+      <SVGclick/>
       <div className="click">클릭</div>
       <div className="tactile">택타일</div>
       <div className="linear">리니어</div>
---
+++ src/components/svgClick.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactSVG } from "react-svg";
+
+export default function KeyBoardSVG() {
+  return (
+    <div className="Keyboard">
+      <ReactSVG src="svg\KeyBoard.svg" />
+    </div>
+  );
+}
