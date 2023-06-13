f8ca2c311d284ec19141587ee6ebf2c117b05e2c
krapli441 <tlfqj1208@gmail.com>
2023-06-13 21:45:14 +0900

    svg id 인식하기

--- src/components/svgClick.jsx
+++ src/components/svgClick.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { ReactSVG } from "react-svg";
 
 export default function KeyBoardSVG() {
+  const svgRef = useRef(null);
+  useEffect(() => {
+    if (svgRef.current) {
+      const svgElement = svgRef.current;
+      console.log(svgElement);
+    }
+  }, []);
+
   return (
-    <div className="Keyboard">
-      <ReactSVG src="svg\KeyBoard.svg" />
+    <div className="KeyboardBox">
+      <ReactSVG
+        src="svg/KeyBoard.svg"
+        beforeInjection={(svg) => (svgRef.current = svg)}
+      />
     </div>
   );
 }
