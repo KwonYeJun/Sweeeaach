dc1889335cac58380ba395defce8385febf34629
gwonyejun <kyjuni1059@gmail.com>
2023-06-15 18:40:32 +0900

    한글 정규식 추가 및 준형씨 svg파일과 병합

--- src/components/svgClick.jsx
+++ src/components/svgClick.jsx
@@ -44,10 +44,15 @@ export default function KeyBoardSVG() {
         // rect 요소에 접근하여 원하는 작업 수행
         console.log('특문', rectElement);
       }
-      else if ((/^[A-Z]$/.test(event))){
-        const svgElement = document.querySelector('svg'); // SVG 컨테이너 요소 선택
-        const rectElement = svgElement.querySelector(`#${event}`); // <rect> 요소 식별
-        // rect 요소에 접근하여 원하는 작업 수행
+      else if (/[\uAC00-\uD7AF]+/.test(event)) {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Korean${event}`);
+        console.log('한글', rectElement);
+      }
+      else if (/^[A-Z]$/.test(event)) {
+        const lowercaseEvent = event.toLowerCase(); // 대문자를 소문자로 변환
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#${lowercaseEvent}`);
         console.log('대문자', rectElement);
       }
       else {
