6f53a9e0189811e77da2c9d2957dd7097e3def73
박준형 <108733795+krapli441@users.noreply.github.com>
2023-06-15 19:09:58 +0900

    키보드를 눌렀다 뗐을 때 svg 색깔이 바뀌도록 수정

--- src/components/svgClick.jsx
+++ src/components/svgClick.jsx
@@ -8,14 +8,12 @@ export default function KeyBoardSVG() {
     if (svgLoaded) {
       const targetElement = document.getElementById("KeyBoard");
       if (targetElement) {
-        console.log(targetElement)
+        console.log(targetElement);
         document.addEventListener("keydown", handleKeyDown);
         document.addEventListener("keyup", handleKeyUp);
-        settest(targetElement)
+        settest(targetElement);
         // 자식들 찾다
         console.log(targetElement.children[2]);
-
-
       }
     }
 
@@ -40,47 +38,46 @@ export default function KeyBoardSVG() {
     if (svgLoaded) {
       if (/^[0-9]$/.test(event)) {
         const number = parseInt(event);
-        const svgElement = document.querySelector('svg'); // SVG 컨테이너 요소 선택
+        const svgElement = document.querySelector("svg"); // SVG 컨테이너 요소 선택
         const rectElement = svgElement.querySelector(`#_${number}`); // <rect> 요소 식별
         // rect 요소에 접근하여 원하는 작업 수행
-        console.log('숫자',rectElement);
-      } 
-      else if ((/^[+=]$/.test(event))){
-        const svgElement = document.querySelector('svg'); // SVG 컨테이너 요소 선택
+        console.log("숫자", rectElement);
+      } else if (/^[+=]$/.test(event)) {
+        const svgElement = document.querySelector("svg"); // SVG 컨테이너 요소 선택
         const rectElement = svgElement.querySelector(`#Num${event}`); // <rect> 요소 식별
         // rect 요소에 접근하여 원하는 작업 수행
-        console.log('특문', rectElement);
-      }
-      else if (/[\uAC00-\uD7AF]+/.test(event)) {
-        const svgElement = document.querySelector('svg');
+        console.log("특문", rectElement);
+      } else if (/[\uAC00-\uD7AF]+/.test(event)) {
+        const svgElement = document.querySelector("svg");
         const rectElement = svgElement.querySelector(`#Korean${event}`);
-        console.log('한글', rectElement);
-      }
-      else if (/^[A-Z]$/.test(event)) {
+        console.log("한글", rectElement);
+      } else if (/^[A-Z]$/.test(event)) {
         const lowercaseEvent = event.toLowerCase(); // 대문자를 소문자로 변환
-        const svgElement = document.querySelector('svg');
+        const svgElement = document.querySelector("svg");
         const rectElement = svgElement.querySelector(`#${lowercaseEvent}`);
-        console.log('대문자', rectElement);
-      }
-      else {
-        const svgElement = document.querySelector('svg'); // SVG 컨테이너 요소 선택
+        console.log("대문자", rectElement);
+      } else {
+        const svgElement = document.querySelector("svg"); // SVG 컨테이너 요소 선택
         const rectElement = svgElement.querySelector(`#${event}`); // <rect> 요소 식별
+        rectElement.style.fill = "white";
         // rect 요소에 접근하여 원하는 작업 수행
-        console.log('test', rectElement);
+        console.log("test", rectElement);
       }
-
     }
   };
   const handleKeyDown = (event) => {
     const pressedKey = event.key;
     console.log(`${pressedKey} 누름`);
-    handleSvgInjection(pressedKey)
-    console.log(test)
+    handleSvgInjection(pressedKey);
+    console.log(test);
     playSound();
   };
   const handleKeyUp = (event) => {
     const pressedKey = event.key;
     console.log(`${pressedKey} 뗐음`);
+    // rect 요소의 style 속성을 제거합니다.
+    const rectElement = document.querySelector(`#${pressedKey}`);
+    rectElement.style.fill = null;
     playSoundTwo();
   };
 
@@ -102,7 +99,6 @@ export default function KeyBoardSVG() {
         src="svg/KeyBoard.svg"
         afterInjection={() => setSvgLoaded(true)}
       />
-
     </div>
   );
 }
