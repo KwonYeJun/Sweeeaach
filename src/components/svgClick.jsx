05ca793a8d60bb61b6602cb660c98e7ef7d8d40b
gwonyejun <kyjuni1059@gmail.com>
2023-06-25 22:21:09 +0900

    5개 빼고 완성

--- src/components/svgClick.jsx
+++ src/components/svgClick.jsx
@@ -391,6 +391,102 @@ export default function KeyBoardSVG() {
         rectElement.style.fill = "#d90429";
         console.log('L_Alt', rectElement);
       } 
+      else if (code === 'NumpadSubtract') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num-`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num-', rectElement);
+      } 
+      else if (code === 'NumpadMultiply') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#_10`);
+        rectElement.style.fill = "#d90429";
+        console.log('_10', rectElement);
+      } 
+      else if (code === 'NumpadDivide') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#_9-2`);
+        rectElement.style.fill = "#d90429";
+        console.log('_9-2', rectElement);
+      }   else if (code === 'NumLock') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#NumLock`);
+        rectElement.style.fill = "#d90429";
+        console.log('NumLock', rectElement);
+      } 
+      else if (code === 'NumpadAdd') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num_`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num_', rectElement);
+      } 
+      else if (code === 'NumpadEnter') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#NumEnter`);
+        rectElement.style.fill = "#d90429";
+        console.log('NumEnter', rectElement);
+      } 
+      else if (code === 'Numpad0') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num0`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num0', rectElement);
+      } 
+      else if (code === 'NumpadDecimal') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num.`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num.', rectElement);
+      } 
+      else if (code === 'Numpad1') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num1`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num1', rectElement);
+      } 
+      else if (code === 'Numpad2') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num2`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num2', rectElement);
+      } 
+      else if (code === 'Numpad3') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num3`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num3', rectElement);
+      } 
+      else if (code === 'Numpad4') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num4`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num4', rectElement);
+      } else if (code === 'Numpad5') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num5`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num5', rectElement);
+      } else if (code === 'Numpad6') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num6`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num6', rectElement);
+      } else if (code === 'Numpad7') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num7`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num7', rectElement);
+      } else if (code === 'Numpad8') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num8`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num8', rectElement);
+      } else if (code === 'Numpad9') {
+        const svgElement = document.querySelector('svg');
+        const rectElement = svgElement.querySelector(`#Num9`);
+        rectElement.style.fill = "#d90429";
+        console.log('Num9', rectElement);
+      } 
       else {
         const svgElement = document.querySelector('svg'); // SVG 컨테이너 요소 선택
         const rectElement = svgElement.querySelector(`#${event}`); // <rect> 요소 식별
@@ -469,6 +565,102 @@ export default function KeyBoardSVG() {
       const rectElement = svgElement.querySelector(`#___`);
       rectElement.style.fill = null;
       console.log('`', rectElement);
+    } 
+    else if (event.code === 'NumpadSubtract') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num-`);
+      rectElement.style.fill = null;
+      console.log('Num-', rectElement);
+    } 
+    else if (event.code === 'NumpadMultiply') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_10`);
+      rectElement.style.fill = null;
+      console.log('_10', rectElement);
+    } 
+    else if (event.code === 'NumpadDivide') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_9-2`);
+      rectElement.style.fill = null;
+      console.log('_9-2', rectElement);
+    }   else if (event.code === 'NumLock') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#NumLock`);
+      rectElement.style.fill = null;
+      console.log('NumLock', rectElement);
+    } 
+    else if (event.code === 'NumpadAdd') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num_`);
+      rectElement.style.fill = null;
+      console.log('Num_', rectElement);
+    } 
+    else if (event.code === 'NumpadEnter') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#NumEnter`);
+      rectElement.style.fill = null;
+      console.log('NumEnter', rectElement);
+    } 
+    else if (event.code === 'Numpad0') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num0`);
+      rectElement.style.fill = null;
+      console.log('Num0', rectElement);
+    } 
+    else if (event.code === 'NumpadDecimal') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num.`);
+      rectElement.style.fill = null;
+      console.log('Num.', rectElement);
+    } 
+    else if (event.code === 'Numpad1') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num1`);
+      rectElement.style.fill = null;
+      console.log('Num1', rectElement);
+    } 
+    else if (event.code === 'Numpad2') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num2`);
+      rectElement.style.fill = null;
+      console.log('Num2', rectElement);
+    } 
+    else if (event.code === 'Numpad3') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num3`);
+      rectElement.style.fill = null;
+      console.log('Num3', rectElement);
+    } 
+    else if (event.code === 'Numpad4') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num4`);
+      rectElement.style.fill = null;
+      console.log('Num4', rectElement);
+    } else if (event.code === 'Numpad5') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num5`);
+      rectElement.style.fill = null;
+      console.log('Num5', rectElement);
+    } else if (event.code === 'Numpad6') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num6`);
+      rectElement.style.fill = null;
+      console.log('Num6', rectElement);
+    } else if (event.code === 'Numpad7') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num7`);
+      rectElement.style.fill = null;
+      console.log('Num7', rectElement);
+    } else if (event.code === 'Numpad8') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num8`);
+      rectElement.style.fill = null;
+      console.log('Num8', rectElement);
+    } else if (event.code === 'Numpad9') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#Num9`);
+      rectElement.style.fill = null;
+      console.log('Num9', rectElement);
     } 
     else if (/^[ㄱ-ㅎ]+/.test(pressedKey)) {
       const svgElement = document.querySelector('svg');
