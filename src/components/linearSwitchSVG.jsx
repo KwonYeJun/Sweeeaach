69c3f3e17af8a12878f58a4310b56ac49ab3377e
krapli441 <tlfqj1208@gmail.com>
2023-07-02 13:24:22 +0900

    svg에 표현되지 않는 일부 애니메이션이 있음

--- src/components/linearSwitchSVG.jsx
+++ src/components/linearSwitchSVG.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ReactSVG } from "react-svg";
-import { Box, Text, Heading } from "@chakra-ui/react";
+import { Box, Text, Heading, Image } from "@chakra-ui/react";
 
 import "../css/keyboard.css";
 
@@ -87,14 +87,35 @@ export default function LinearKeyBoardSVG() {
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <Box className="keyboardInformationBox">
-        <Heading
-          fontSize={"140px"}
-          color={"#DB3A34"}
-          fontFamily={"Staatliches"}
-        >
-          LINEAR
-        </Heading>
+      <Box className="keyboardInformationBox" display={"flex"} width={"50%"}>
+        <Box className="keyboardMechanismGIF" width={"20%"}>
+          <Image src="/img/clickSwitch.gif" boxSize={"100%"}></Image>
+        </Box>
+        <Box className="switchInformation" width={"80%"}>
+          <Heading
+            className="switchTitle"
+            fontSize={"140px"}
+            color={"#DB3A34"}
+            fontFamily={"Staatliches"}
+          >
+            LINEAR
+          </Heading>
+          <Text>
+            택타일 스위치와 다르게 스템의 돌기가 선형적(Linear), 즉 올곧은
+            직선이기 때문에 별도의 촉각적 걸림 없이 그대로 바닥까지 내려가는
+            스위치이다. 대표적인 스위치는 적축(MX Red), 흑축(MX Black), 스피드
+            은축(MX Speed Silver ). 저소음 적축과 저소음 흑축도 있다.
+            진회축(Dark Grey, 혹은 Linear Grey)도 존재하지만 체리 홈페이지에는
+            등재되지 않았다. 택타일 스위치인 회축(Grey)의 스프링을 사용한 리니어
+            스위치인데, 그 무겁다는 흑축보다 무거워서 실사용하기엔 버거운 수준.
+            때문에 상당히 희귀하다. 체리사에서 Special 스위치로 분류하는
+            스위치중에 CHERRY MX BLACK CLEAR-TOP이란 버전도 있는데, 일명 닉시(MX
+            Nixie)라고 불리우는 스위치이다. 1980년대 Nixdorf Computer AG에
+            납품하기 위해 특별히 제조된 흑축 스위치인데, 커뮤니티에서 인기가
+            많아서 2022년 복각되었다. 클리어 뚜껑색과 금색 스프링이 특징. 압력도
+            조금 더 높다.
+          </Text>
+        </Box>
       </Box>
       <Box className="Keyboard" width={"50vw"}>
         <ReactSVG
