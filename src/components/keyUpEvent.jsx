2952065eadf12d8f6bb0575c32a82a1cf084d1eb
gwonyejun <kyjuni1059@gmail.com>
2023-07-02 14:30:32 +0900

    상단에 있는 숫자 특분 완료 백틱 하기 전 버전

--- src/components/keyUpEvent.jsx
+++ src/components/keyUpEvent.jsx
@@ -153,12 +153,144 @@ export function KeyUpEvent(event, code,svgLoaded) {
       console.log('Num9', rectElement);
     } 
     else if (/^[A-Z]$/.test(event)) {
-      const lowercasepressedKey = event.toLowerCase(); // 대문자를 소문자로 변환
+      const lowercaseevent = event.toLowerCase(); // 대문자를 소문자로 변환
       const svgElement = document.querySelector("svg");
-      const rectElement = svgElement.querySelector(`#${lowercasepressedKey}`);
+      const rectElement = svgElement.querySelector(`#${lowercaseevent}`);
       rectElement.style.fill = null;
       console.log('대문자', rectElement);
     }
+    // 특문 오른쪽 알트
+    else if (event === '!') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_1`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '@') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_2`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '#') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_3`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '$') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_4`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '%') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_5`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '^') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_6`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '&') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_7`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '*') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_8`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '(') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_9`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === ')') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_0`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '_') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_-`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    } else if (event === '+' || event === '=') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_2-2`);
+      rectElement.style.fill = null;
+      console.log('특수문자', rectElement);
+    }
+    else if (code === 'Backquote') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_`);
+      rectElement.style.fill = null;
+      console.log('`', rectElement);
+    } 
+    else if (event === '[' || event === '{') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_2-2`);
+      rectElement.style.fill = null;
+      console.log('[', rectElement);
+    }
+    else if (event === ';' || event === ':') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_3-2`);
+      console.log(';', rectElement);
+      rectElement.style.fill = null;
+    }
+
+    else if (event === 'HangulMode') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#R_Alt`);
+      rectElement.style.fill = null;
+      console.log('오른쪽 Alt', rectElement);
+    }
+    else if (event === '-') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_-`);
+      console.log('-', rectElement);
+      rectElement.style.fill = null;
+    }
+    else if (event === ']' || event === '}') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`[data-name="]"]`);
+      rectElement.style.fill = null;
+      console.log(']', rectElement);
+    }
+    else if (event === ',' || event === '<') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_4-2`);
+      console.log(',', rectElement);
+      rectElement.style.fill = null;
+    }
+    else if (event === '.' || event === '>') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`[data-name="."]`);
+      console.log('.', rectElement);
+      rectElement.style.fill = null;
+    }
+    else if (event === '?' || event === '/') {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_5-2`);
+      console.log(',', rectElement);
+      rectElement.style.fill = null;
+    }
+    else if (event === '\\' || event === "|") {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_8-2`);
+      console.log('\\', rectElement);
+      rectElement.style.fill = null;
+    }
+    else if (event === '"' || event === "'") {
+      const svgElement = document.querySelector('svg');
+      const rectElement = svgElement.querySelector(`#_6-2`);
+      console.log('\\', rectElement);
+      rectElement.style.fill = null;
+    }
+
+
+
     else{
       const svgElement = document.querySelector("svg");
       const rectElement = svgElement.querySelector(`#${event}`);
